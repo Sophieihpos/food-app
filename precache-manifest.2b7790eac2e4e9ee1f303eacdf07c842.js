aa6ed32a85c339ee328683054d5ede5fa9df7b1e
Sophieihpos <sophie.emma.chapman@gmail.com>
2020-03-17 06:03:59 +1100

    Updates

---
+++ precache-manifest.2b7790eac2e4e9ee1f303eacdf07c842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec4121b2dd8c8e05dc0094be4a3afe34",
+    "url": "/food-app/index.html"
+  },
+  {
+    "revision": "22562b5af16c9ace63af",
+    "url": "/food-app/static/css/main.f76137ab.chunk.css"
+  },
+  {
+    "revision": "7c8dae46a7bb32cba663",
+    "url": "/food-app/static/js/2.ab95d561.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/food-app/static/js/2.ab95d561.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22562b5af16c9ace63af",
+    "url": "/food-app/static/js/main.692c20ad.chunk.js"
+  },
+  {
+    "revision": "7e9e2528e78390d41cba",
+    "url": "/food-app/static/js/runtime-main.0b25c588.js"
+  }
+]);
